a02fa28d0b926ceb7916f0d08d0b0b2cfc0d3d41
Alma Guerrero Sánchez <31255851+almaguerrero@users.noreply.github.com>
2023-12-22 11:28:50 -0600

    1

---
+++ ejercicio_01.py
@@ -0,0 +1,8 @@
+# hacer un programa que solicite dos números 
+# y muestre a suma, resta, multitplcacion y divisición
+num_1 = input('Dame el primer número:  ')
+num_2 = input('Dame el segundo número:  ')
+print("la suma es: ", int(num_1)+int(num_2))
+print("la resta es: ", int(num_2)-int(num_1))
+print("la mult es: ", int(num_1)*int(num_2))
+print("la div es: ", int(num_2)/int(num_1))
